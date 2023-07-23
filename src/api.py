2ef299bd614ac40a27102dd25f89acf106854600
Axel Ivanda Tanjung <axel.tanjung@gmail.com>
2023-07-23 23:15:18 +0700

    setting host api

--- src/api.py
+++ src/api.py
@@ -80,4 +80,4 @@ def predict(data: api_data):
     #return {"res" : y_pred, "error_msg": ""}
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host = "0.0.0.0", port = 8080)
+    uvicorn.run("api:app", host = "34.212.254.202", port = 8080)
